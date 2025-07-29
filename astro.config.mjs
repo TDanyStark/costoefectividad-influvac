7892dfba3be532114466b3f8eace811b1170fe12
Stark Industries <97692264+TDanyStark@users.noreply.github.com>
2025-07-29 18:34:27 -0500

    Primer commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+import path from 'path';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  base: '/apps/abbott/costoefectividad-influvac',
+  outDir: 'apps/abbott/costoefectividad-influvac',
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'),
+      }
+    }
+  },
+});
