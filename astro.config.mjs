d71537e784664f8f71acd65722654912b44032cb
Stark Industries <97692264+TDanyStark@users.noreply.github.com>
2025-08-12 11:16:42 -0500

    refactor: remove Calculator component from calculadora.astro and clean up tsconfig.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import path from 'path';
 
-import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
@@ -18,6 +17,4 @@ export default defineConfig({
       }
     }
   },
-
-  integrations: [react()],
 });
